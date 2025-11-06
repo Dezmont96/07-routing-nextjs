4aa998e744b4d687f582a8fc1bfe6ef8941482ef
Dezmont96 <dezmontmayals@gmail.com>
2025-11-07 01:11:29 +0200

    fix modal

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -7,11 +7,9 @@ import { notFound } from 'next/navigation';
 import { fetchNoteById } from '@/lib/api';
 import ModalNotePreviewClient from './NotePreview.client';
 
-// 1. Змінюємо тип, щоб вказати, що params - це Promise
 type Props = { params: Promise<{ id: string }> };
 
 export default async function NoteModalPage({ params }: Props) {
-  // 2. Додаємо `await` для отримання значення з Promise
   const { id } = await params;
   
   const queryClient = new QueryClient();
